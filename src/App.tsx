c2f2fba5f1325e3e74f8fb5addd0ebf379d8d205
mirko2004 <mirkotarantolavoro2004@gmail.com>
2025-06-07 17:13:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import SecondVideo from "./pages/SecondVideo";
 import NotFound from "./pages/NotFound";
+import InstagramLandingPage from "./components/InstagramLandingPage";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +16,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <InstagramLandingPage />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/secondo-video" element={<SecondVideo />} />
