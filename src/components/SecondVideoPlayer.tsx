7e497f7789b2eacb376dc4212dd6c7263151b234
mirko2004 <mirkotarantolavoro2004@gmail.com>
2025-06-07 10:12:00 +0200

    Update SecondVideoPlayer.tsx

--- src/components/SecondVideoPlayer.tsx
+++ src/components/SecondVideoPlayer.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState, useEffect, useCallback } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import { NodeJS } from 'node';
 import { Play, Pause, Volume2, VolumeX, Maximize, Minimize } from "lucide-react";
 import FinalPopup from "./FinalPopup";
 
