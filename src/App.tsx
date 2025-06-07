3112154e269519b06e5178f4eff2a1c819027160
mirko2004 <mirkotarantolavoro2004@gmail.com>
2025-06-07 17:22:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import SecondVideo from "./pages/SecondVideo";
 import NotFound from "./pages/NotFound";
-import InstagramLandingPage from "../src/components/InstagramLandingPage";
+import InstagramLandingPage from "./components/InstagramLandingPage";
 
 const queryClient = new QueryClient();
 
@@ -30,3 +30,4 @@ const App = () => (
 );
 
 export default App;
+
